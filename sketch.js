7e443cabe2591801b16720a475aadb0723ef35ca
clarkrosen <clark.rosen24@gmail.com>
2016-01-25 14:49:18 -0500

    Draft 1

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+   	createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  	  fill(0,150,150);0
+  	ellipse(130, 170, 250, 250);
+  	
+  	fill(0,50,250);0
+  	ellipse(130, 170, 240, 240);
+  	
+  	fill(0,150,250);
+  	ellipse(190, 170, 100, 100);
+  	
+  	fill(0,0,150);
+  	ellipse(170, 180, 40, 40);
+  	
+  	fill(0,150,250);
+  	ellipse(70, 170, 100, 100);
+  	
+  	fill(0,0,150);
+  	ellipse(90, 180, 40, 40);
+  	
+  	fill(250,150,150);
+  	ellipse(90, 180, 30, 30);
+  	
+  	fill(250,150,150);
+  	ellipse(170, 180, 30, 30);
+
+  	fill(0,0,0);
+  	noStroke();
+    arc(120, 230, 70, 60, 0, PI);
+
+  	
+}
