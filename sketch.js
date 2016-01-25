cb3919f9dc2c41e6241a511180115a0f72d7906c
clarkrosen <clark.rosen24@gmail.com>
2016-01-25 15:27:23 -0500

    Draft 3

--- sketch.js
+++ sketch.js
@@ -1,47 +1,49 @@
+
 function setup() {
-   	createCanvas(windowWidth, windowHeight);
+   	createCanvas(displayWidth, displayHeight);
 
 }
 
 function draw() {
     	  background(51);
 
-  
-  	fill(250,250,250);
-  	ellipse(130, 170, 250, 250);
+    fill(250,250,250);
+  	ellipse(330, 170, 250, 250);
   	
   	fill(250,250,150);
-  	ellipse(130, 170, 240, 240);
+  	ellipse(330, 170, 240, 240);
   	
   	fill(250,250,250);
   	stroke(0,0,0);
-  	ellipse(190, 170, 100, 100);
+  	ellipse(390, 170, 100, 100);
   	
   	fill(100,200,250);
-  	ellipse(170, 180, 40, 40);
+  	ellipse(370, 180, 40, 40);
   	
   	fill(250,250,250);
   	stroke(0,0,0);
-  	ellipse(70, 170, 100, 100);
+  	ellipse(270, 170, 100, 100);
   	
   	fill(100,200,250);
-  	ellipse(90, 180, 40, 40);
+  	ellipse(290, 180, 40, 40);
   	
   	fill(0,220,150);
-  	ellipse(90, 180, 30, 30);
+  	ellipse(290, 180, 30, 30);
   	
   	fill(0,220,150);
-  	ellipse(170, 180, 30, 30);
+  	ellipse(370, 180, 30, 30);
 
   	fill(0,0,0);
-  	ellipse(170, 180, 17, 17);
+  	ellipse(370, 180, 17, 17);
   	
   	 fill(0,0,0);
-  	ellipse(90, 180, 17, 17);
+  	ellipse(290, 180, 17, 17);
   	
   	fill(0,0,0);
   	noStroke();
-    arc(130, 230, 70, 60, 0, PI);
-
-  	
+    arc(330, 230, 70, 60, 0, PI);
+    
+    fill(250,0,0);
+  	noStroke();
+    arc(320, 230, 40, 30, 0, PI);
 }
