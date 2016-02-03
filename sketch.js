df58ad48163c45365380015141966cdb16e7d05e
clarkrosen <clark.rosen24@gmail.com>
2016-02-03 14:37:14 -0500

    Draft 5

--- sketch.js
+++ sketch.js
@@ -1,31 +1,12 @@
-
-function setup() {
-   	createCanvas(displayWidth, displayHeight);
-
-}
-
-function draw() {
-    	  background(51);
-
-    fill(250,250,250);
-  	ellipse(330, 170, 250, 250);
-  	
-  	fill(250,250,150);
-  	ellipse(330, 170, 240, 240);
-  	
-  	fill(250,250,250);
-  	stroke(0,0,0);
-  	ellipse(390, 170, 100, 100);
-  	
   	fill(100,200,250);
-  	ellipse(370, 180, 40, 40);
+  	ellipse(370, 180, 50, 50);
   	
   	fill(250,250,250);
   	stroke(0,0,0);
   	ellipse(270, 170, 100, 100);
   	
   	fill(100,200,250);
-  	ellipse(290, 180, 40, 40);
+  	ellipse(290, 180, 50, 50);
   	
   	fill(0,220,150);
   	ellipse(290, 180, 30, 30);
@@ -52,4 +33,52 @@ function draw() {
     fill(250,0,0);
   	noStroke();
     arc(320, 230, 40, 30, 0, PI);
+    }
+    
+    if (mouseY > 270 && mouseX > 270) {
+    background(0,100,255);
+      
+    fill(250,250,250);
+  	ellipse(330, 170, 250, 250);
+  	
+  	fill(250,250,150);
+  	ellipse(330, 170, 240, 240);
+  	
+  	fill(250,250,250);
+  	stroke(0,0,0);
+  	ellipse(390, 170, 100, 100);
+  	
+  	fill(0,250,250);
+  	ellipse(370, 180, 40, 40);
+  	
+  	fill(250,250,250);
+  	stroke(0,0,0);
+  	ellipse(270, 170, 100, 100);
+  	
+  	fill(0,250,250);
+  	ellipse(290, 180, 40, 40);
+  	
+  	fill(0,200,250);
+  	ellipse(290, 180, 30, 30);
+  	
+  	fill(0,200,250);
+  	ellipse(370, 180, 30, 30);
+
+  	fill(0,0,0);
+  	ellipse(370, 180, 17, 17);
+  	
+  	 fill(0,0,0);
+  	ellipse(290, 180, 17, 17);
+  	
+  	fill(250,250,250);
+  	ellipse(290, 184, 7, 7);
+  	
+  	fill(250,250,250);
+  	ellipse(370, 184, 7, 7);
+  	
+  	fill(0,0,0);
+  	noStroke();
+    arc(330, 240, 50, 40, 0, PI+QUARTER_PI, OPEN);
+    
+    }
 }
