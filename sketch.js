fc3930009543ec4320277ed831c98b0f90fc2900
clarkrosen <clark.rosen24@gmail.com>
2016-02-03 14:40:26 -0500

    Draft 6

--- sketch.js
+++ sketch.js
@@ -1,3 +1,115 @@
+function setup() {
+   	createCanvas(displayWidth, displayHeight);
+
+}
+
+function draw() {
+    	  background(51);
+
+    fill(250,250,250);
+  	ellipse(330, 170, 250, 250);
+  	
+  	fill(250,250,150);
+  	ellipse(330, 170, 240, 240);
+  	
+  	fill(250,250,250);
+  	stroke(0,0,0);
+  	ellipse(390, 170, 100, 100);
+  	
+  	fill(100,200,250);
+  	ellipse(370, 180, 40, 40);
+  	
+  	fill(250,250,250);
+  	stroke(0,0,0);
+  	ellipse(270, 170, 100, 100);
+  	
+  	fill(100,200,250);
+  	ellipse(290, 180, 40, 40);
+  	
+  	fill(0,220,150);
+  	ellipse(290, 180, 30, 30);
+  	
+  	fill(0,220,150);
+  	ellipse(370, 180, 30, 30);
+
+  	fill(0,0,0);
+  	ellipse(370, 180, 17, 17);
+  	
+  	 fill(0,0,0);
+  	ellipse(290, 180, 17, 17);
+  	
+  	fill(250,250,250);
+  	ellipse(290, 184, 7, 7);
+  	
+  	fill(250,250,250);
+  	ellipse(370, 184, 7, 7);
+  	
+  	fill(0,0,0);
+  	noStroke();
+    arc(330, 230, 70, 60, 0, PI);
+    
+    fill(250,0,0);
+  	noStroke();
+    arc(320, 230, 40, 30, 0, PI);
+    
+    if (mouseY > 270 && mouseX < 270) {
+    background(255,0,0);
+    
+    fill(250,250,250);
+  	ellipse(330, 170, 250, 250);
+  	
+  	fill(250,250,150);
+  	ellipse(330, 170, 240, 240);
+  	
+      fill(250,250,250);
+  	stroke(0,0,0);
+  	ellipse(390, 170, 100, 100);
+  	
+  	fill(250,0,0);
+  	ellipse(370, 180, 40, 40);
+  	
+  	fill(250,250,250);
+  	stroke(0,0,0);
+  	ellipse(270, 170, 100, 100);
+  	
+  	fill(250,0,0);
+  	ellipse(290, 180, 40, 40);
+  	
+  	fill(200,0,200);
+  	ellipse(290, 180, 30, 30);
+  	
+  	fill(200,0,200);
+  	ellipse(370, 180, 30, 30);
+
+  	fill(0,0,0);
+  	ellipse(370, 180, 17, 17);
+  	
+  	fill(0,0,0);
+  	ellipse(290, 180, 17, 17);
+  	
+  	fill(250,250,250);
+  	ellipse(290, 184, 7, 7);
+  	
+  	fill(250,250,250);
+  	ellipse(370, 184, 7, 7);
+  	
+  	fill(0,0,0);
+  	ellipse(330, 250, 45, 45);
+    }
+    
+    if (mouseY < 270 && mouseX > 270) {
+    background(0,255,100);
+    
+    fill(250,250,250);
+  	ellipse(330, 170, 250, 250);
+  	
+  	fill(250,250,150);
+  	ellipse(330, 170, 240, 240);
+  	
+  	fill(250,250,250);
+  	stroke(0,0,0);
+  	ellipse(390, 170, 100, 100);
+  	
   	fill(100,200,250);
   	ellipse(370, 180, 50, 50);
   	
